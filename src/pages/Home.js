35217ff65ecffde80668f246ddfd2dfe0679d892
shogun187 <53176307+shogun187@users.noreply.github.com>
2023-02-20 14:38:28 +0800

    Updated home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,13 @@
 import PageContent from '../components/PageContent';
 
 function HomePage() {
-  return (
-    <PageContent title="Welcome!">
-      <p>Welcome to the Task Manager Application!</p>
-    </PageContent>
-  );
+    return (
+        <PageContent title="Welcome!">
+            <p>Welcome to the Task Manager Application!</p>
+            <footer>This app was designed by Shaugn Tan, a Y2 Computer Science student from NUS.</footer>
+        </PageContent>
+
+    );
 }
 
 export default HomePage;
